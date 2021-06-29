7f7c59be44a4cdc98284f666270bd87ca02da83b
StuartBoynton1 <68314809+StuartBoynton1@users.noreply.github.com>
2021-06-29 09:05:58 +0200

    Update lqr_sdp.py

--- lqr_sdp.py
+++ lqr_sdp.py
@@ -13,30 +13,34 @@ def lqr_sdp(N,s_star,gamma_s,gamma_v,gamma_u,AV_number):
     epsilon = 1e-5
     
     H = np.identity(n)
-    H[1:n:2][1:n:2] = 0
+    for i in range(0,N):
+        H[2*i,2*i] = 0
     
     
-    X = cp.Variable((n,n), symmetric=True)
-    Z = cp.Variable((m,n))
-    Y = cp.Variable((m,m), symmetric=True)
+    #X = cp.Variable((n,n), symmetric=True)
+    #Z = cp.Variable((m,n))
+    #Y = cp.Variable((m,m), symmetric=True)
     W = cp.Variable((m+n,m+n), symmetric=True)
+    #                W[0:m,0:m] == Y,
+    #                W[m:,0:m] == Z,
+    #                W[m:,m:] == X,
     
-    objective = cp.Minimize( cp.trace( Q@X ) + cp.trace( R@Y ) )  
-    constraints = [-((A@X - B@Z) + (A@X - B@Z).T + H@(H).T) >= 0, 
-                   X >= epsilon*np.identity(n),
-                   W[0:m,0:m] == Y,
-                   W[m:,0:m] == Z,
-                   W[m:,m:] == X,
-                   W >= 0]
+    objective    = cp.Minimize( cp.trace( Q@W[m:,m:] ) + cp.trace( R@W[0:m,0:m] ) )
+    constraints  = [(A@W[m:,m:] - B@W[0:m,m:]) + (A@W[m:,m:] - B@W[0:m,m:]).T + H@(H).T << 0]
+    constraints += [ W[m:,m:] - epsilon*np.identity(n) >> 0]
+    constraints += [W >> 0]
+    
+    # constraints  = [-((A@X - B@Z) + (A@X - B@Z).T + H@(H).T) >> 0]
+    # constraints += [X - epsilon*np.identity(n) >> 0]
+    # constraints += [np.block([[Y, Z], [Z.T, X]]) >> 0]
     
     prob = cp.Problem(objective, constraints)
+    prob.solve(solver = cp.MOSEK)
     
-    prob.solve(solver=cp.MOSEK)
-    Xd = X.value
-    Zd = Z.value
-    Pd = prob.value
+    Xd = W[m:,m:].value
+    Zd = W[0:m,m:].value
 
-    K = Zd*np.linalg.inv(Xd)   
-
+    
+    K = Zd@np.linalg.inv(Xd)
+    
     return K
-    
